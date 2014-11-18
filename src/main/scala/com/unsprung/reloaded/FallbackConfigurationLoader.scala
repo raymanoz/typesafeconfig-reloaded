ed070c180e893543496e3d7f337ca9fa3cfd689e
Raymond Barlow <raymond.barlow@barclays.com>
2014-11-18 17:10:58 +0000

    Corina, Raymond: Added the actual code!

---
+++ src/main/scala/com/unsprung/reloaded/FallbackConfigurationLoader.scala
@@ -0,0 +1,18 @@
+package com.unsprung.reloaded
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory.{defaultOverrides, parseResourcesAnySyntax}
+import com.typesafe.config.ConfigResolveOptions.defaults
+import com.typesafe.config.impl.ConfigImpl
+
+object FallbackConfigurationLoader {
+  def load(environment: String, loader: ClassLoader = getClass.getClassLoader): Config = {
+    def loadConfig(resourceBasename: String) = parseResourcesAnySyntax(loader, resourceBasename + ".conf")
+
+    def configs(config: Config): List[Config] = config :: (if (config.hasPath("fallback")) configs(loadConfig(config.getString("fallback"))) else Nil)
+
+    ConfigImpl.reloadSystemPropertiesConfig()
+
+    (defaultOverrides(loader) :: configs(loadConfig(environment))).reduce(_ withFallback _).resolve(defaults())
+  }
+}
---
+++ src/test/scala/com/unsprung/reloaded/FallbackConfigurationLoaderTest.scala
@@ -0,0 +1,65 @@
+package com.unsprung.reloaded
+
+import java.io.File
+
+import com.googlecode.totallylazy.Files.write
+import com.googlecode.totallylazy.Strings.bytes
+import com.typesafe.config.ConfigException.UnresolvedSubstitution
+import org.scalatest.FunSuite
+
+class FallbackConfigurationLoaderTest extends FunSuite {
+  private def writeConfig(configFilename: String, content: String) = write(bytes(content), new File("target/scala-2.11/classes/" + configFilename))
+
+  test("can get values from a config with fallback specified") {
+    writeConfig("ref.conf", "var1=sheep")
+    writeConfig("override.conf",
+      """fallback=ref
+        |var2=cheese""".stripMargin)
+
+    assert(FallbackConfigurationLoader.load("override").getString("var2") === "cheese")
+  }
+
+  test("can get reference values from a config with fallback specified") {
+    writeConfig("ref.conf", "var1=sheep")
+    writeConfig("override.conf",
+      """fallback=ref
+        |var2=cheese""".stripMargin)
+
+    assert(FallbackConfigurationLoader.load("override").getString("var1") === "sheep")
+  }
+
+  test("can get structured values") {
+    writeConfig("config.conf",
+      """lookup {
+        |  value=puppies
+        |}""".stripMargin)
+
+    assert(FallbackConfigurationLoader.load("config").getString("lookup.value") === "puppies")
+  }
+
+  test("variables used in reference, and defined in reference, can be overriden in later configs") {
+    writeConfig("ref.conf",
+      """var=sheep
+        |value=${var}""".stripMargin)
+    writeConfig("override.conf",
+      """fallback=ref
+        |var=cheese""".stripMargin)
+
+    assert(FallbackConfigurationLoader.load("override").getString("value") === "cheese")
+  }
+
+  test("cat get system property values") {
+    System.setProperty("jabberwocky", "Jubjub bird")
+    writeConfig("config.conf", "value=${jabberwocky}")
+
+    assert(FallbackConfigurationLoader.load("config").getString("value") === "Jubjub bird")
+  }
+
+  test("cannot load if variable subsitution fails") {
+    writeConfig("config.conf", "var=${missing}")
+
+    intercept[UnresolvedSubstitution] {
+      FallbackConfigurationLoader.load("config")
+    }
+  }
+}
