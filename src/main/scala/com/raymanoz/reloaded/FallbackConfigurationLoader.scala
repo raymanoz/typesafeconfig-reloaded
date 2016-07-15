062db36ae9110ba7a07a52bcc2767cbc03233f28
Stacy Curl <stacy.curl@barclays.com>
2016-07-15 18:05:33 +0100

    Refactored FallbackConfigurationLoader to be able to specify the 'last-chance' fallback
    Also made loadOrDie pass along the 'loader' which was effectively unused before.

--- src/main/scala/com/raymanoz/reloaded/FallbackConfigurationLoader.scala
+++ src/main/scala/com/raymanoz/reloaded/FallbackConfigurationLoader.scala
@@ -8,18 +8,18 @@ import com.typesafe.config.impl.ConfigImpl
 import scala.util.Try
 
 object FallbackConfigurationLoader {
-  def load(environment: String, loader: ClassLoader = getClass.getClassLoader): Config = {
+  def load(environment: String, loader: ClassLoader = getClass.getClassLoader, default: ClassLoader ⇒ Config = defaultOverrides): Config = {
     def loadConfig(resourceBasename: String) = parseResourcesAnySyntax(loader, resourceBasename + ".conf", ConfigParseOptions.defaults().setAllowMissing(false))
 
     def configs(config: Config): List[Config] = config :: (if (config.hasPath("fallback")) configs(loadConfig(config.getString("fallback"))) else Nil)
 
     ConfigImpl.reloadSystemPropertiesConfig()
 
-    (defaultOverrides(loader) :: configs(loadConfig(environment))).reduce(_ withFallback _).resolve(defaults())
+    (default(loader) :: configs(loadConfig(environment))).reduce(_ withFallback _).resolve(defaults())
   }
 
-  def loadOrDie(environment: String, loader: ClassLoader = getClass.getClassLoader): Config =
-    Try(FallbackConfigurationLoader.load(environment)).recover {
+  def loadOrDie(environment: String, loader: ClassLoader = getClass.getClassLoader, default: ClassLoader ⇒ Config = defaultOverrides): Config =
+    Try(FallbackConfigurationLoader.load(environment, loader, default)).recover {
       case e: Throwable =>
         e.printStackTrace()
         sys.exit(-1)
