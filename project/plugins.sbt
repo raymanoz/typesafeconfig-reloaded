6253c26de797a664dd32c1d96d9e594da606b053
Julien Truffaut <truffaut.julien@gmail.com>
2016-11-14 15:11:54 +0000

    upgrade to scala 2.12

--- project/plugins.sbt
+++
@@ -1,5 +0,0 @@
-resolvers += "raymanoz repo" at "http://repo.raymanoz.com/"
-
-resolvers += "Bodar Repo" at "http://repo.bodar.com/"
-
-addSbtPlugin("com.raymanoz.shavenmaven-sbtplugin" %% "shavenmaven-sbtplugin" % "2")
--- travis_build.sh
+++ travis_build.sh
@@ -14,9 +14,9 @@ cd target
 
 function prepare_for_publish() {
     mkdir s3
-    cp com/raymanoz/reloaded/reloaded_2.11/${version}/*${version}.jar s3
-    cp com/raymanoz/reloaded/reloaded_2.11/${version}/*${version}-sources.jar s3
-    cp com/raymanoz/reloaded/reloaded_2.11/${version}/*${version}.pom s3
+    cp com/raymanoz/reloaded/reloaded_2.12/${version}/*${version}.jar s3
+    cp com/raymanoz/reloaded/reloaded_2.12/${version}/*${version}-sources.jar s3
+    cp com/raymanoz/reloaded/reloaded_2.12/${version}/*${version}.pom s3
 }
 
 prepare_for_publish || moan 'Failed to prepare_for_publish'
